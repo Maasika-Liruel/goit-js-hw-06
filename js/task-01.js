5cf33b1d16d80b02d4cd5c5a449a548f3fe91e9d
Maasika-Liruel <135087363+Maasika-Liruel@users.noreply.github.com>
2023-09-28 20:33:03 -0400

    task01

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,11 @@
 const categories = document.querySelectorAll("li.item");
 console.log("Number of categories: ", categories.length);
+
+categories.forEach(function (item) {
+    const name = item.querySelector("h2");
+    const numberName = item.querySelectorAll("li");
+
+    console.log("Category:", name.textContent);
+    console.log("Elements:", numberName.length);
+
+})
